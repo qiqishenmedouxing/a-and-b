a352f40a5907c914a35f7eae7a2c82e8a7a05f9b
qiqishenmedouxing <176547035+qiqishenmedouxing@users.noreply.github.com>
2025-01-16 20:53:25 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int n;
-  while(cin>>n)
-  {
-      while(n--)
-      {
-          int a,b;
-          cin>>a>>b;
-          cout<<a+b<<endl;
-      }
-  }
+int main()
+{
+   int n,a,b;
+while(cin>>n)
+{
+    for (int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
     return 0;
 }
